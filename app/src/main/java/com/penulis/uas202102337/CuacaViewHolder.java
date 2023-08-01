13e76f64000865184c58a52421c04afea85bb3bd
202102337 <92079267+cyrillatara@users.noreply.github.com>
2023-08-01 11:19:22 +0700

    Initial commit

---
+++ app/src/main/java/com/penulis/uas202102337/CuacaViewHolder.java
@@ -0,0 +1,20 @@
+package com.penulis.uas202102337;
+
+import android.view.View;
+import android.widget.TextView;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+public class CuacaViewHolder extends RecyclerView.ViewHolder {
+    public TextView namaTextView, deskripsiTextView, tglWaktuTextView, suhuTextView;
+
+    public CuacaViewHolder(View itemView) {
+        super(itemView);
+
+        namaTextView = (TextView) itemView.findViewById(R.id.namaTextView);
+        deskripsiTextView = (TextView) itemView.findViewById(R.id.deskripsiTextView);
+        tglWaktuTextView = (TextView) itemView.findViewById(R.id.tglWaktuTextView);
+        suhuTextView = (TextView) itemView.findViewById(R.id.suhuTextView);
+    }
+}
+
